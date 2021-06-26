d061e50a186528abfd525ce86ebe3abb72192d23
trinhvanminh <minh.a1.no.0@gmail.com>
2021-06-26 21:15:11 +0700

    add animation + js

---
+++ BEM/toast message/toast.js
@@ -0,0 +1,51 @@
+// toast function
+function toast({title = '', message = '', type = 'info', duration = 3000}) {
+    const main = document.getElementById('toast');
+    if (main) {
+        const toast = document.createElement('div');
+
+        //auto remove toast
+        const autoRemoveId = setTimeout(function() {
+            main.removeChild(toast);
+        }, duration + 1000);
+
+        //remove toast when clicked
+        toast.onclick = function(e) {
+            if(e.target.closest('.toast__close')){
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            }
+        }
+
+
+        const icons = {
+            info: 'fas fa-info-circle',
+            warning: 'fas fa-exclamation-circle',
+            success: 'fas fa-check-circle'
+        };
+
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+
+        toast.classList.add('toast',`toast--${type}`);
+        toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+        
+        
+        toast.innerHTML = `
+            <div class="toast__icon">
+                <i class="${icon}"></i>
+            </div>
+            <div class="toast__body">
+                <h3 class="toast__title">${title}</h3>
+                <p class="toast__msg">${message}</p>
+            </div>
+            <div class="toast__close">
+                <i class="fas fa-times"></i>
+            </div>
+            
+        `;
+        main.appendChild(toast);
+
+        
+    }
+}
