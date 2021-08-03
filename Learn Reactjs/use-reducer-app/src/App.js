5e38dbf7a30e7d426fbcb38b3549cbe46d6b8980
trinhvanminh <minh.a1.no.0@gmail.com>
2021-08-03 18:39:59 +0700

    update

--- Learn Reactjs/use-reducer-app/src/App.js
+++ Learn Reactjs/use-reducer-app/src/App.js
@@ -1,8 +1,18 @@
-
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-	return <div className="App">Learn React</div>;
+	return (
+		<div className="App">
+			<Router>
+				<NavBar />
+				<Switch>
+					<Route path="/" exact />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
